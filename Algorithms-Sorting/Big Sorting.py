9ba9518b444f942e2cf21a703fd3e197a8e83afe
Liang Hong <hoilus@users.noreply.github.com>
2017-12-22 21:01:30 -0600

    Create Big Sorting.py

---
+++ Algorithms-Sorting/Big Sorting.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+
+import sys
+
+
+n = int(input().strip())
+unsorted = []
+unsorted_i = 0
+for unsorted_i in range(n):
+   unsorted_t = str(input().strip())
+   unsorted.append(unsorted_t)
+# your code goes here
+# unsorted.sort(key=lambda x: len(x))
+unsorted.sort(key=int)
+for i in unsorted:
+    print(i)
